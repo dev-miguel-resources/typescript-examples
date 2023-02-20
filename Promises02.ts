5db5d49c68f89d353590ed5e24754e950702b1b5
dev-miguel-resources <ingenieromiguelch@gmail.com>
2023-02-20 14:12:11 -0300

    promises with async/await

--- Promises02.ts
+++ Promises02.ts
@@ -1,1 +1,17 @@
-// Promises: Async/Await
+// Promises: Async/Await (Syntax Sugar)
+const executePromise = async () => {
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("resolve executed with async/await")
+        }, 3000)
+    })
+
+    try {
+        const resolvePromise = await promise
+        console.log(resolvePromise)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+executePromise()
